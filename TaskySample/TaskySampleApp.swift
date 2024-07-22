ce078cd88885fe2343af7693a160ca2049df24e7
Suat Karakusoglu <suat.karakusoglu@d-teknoloji.com.tr>
2024-07-22 16:14:01 +0300

    Initial Commit

---
+++ TaskySample/TaskySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskySampleApp.swift
+//  TaskySample
+//
+//  Created by Suat Karakusoglu (Dogus Teknoloji) on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
